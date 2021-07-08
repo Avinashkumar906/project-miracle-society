c9d68d8bfa0fb01c6d64982e9561f0b5d1a0fc7c
avinashparsad906 <avinash.parsad@osganalytics.com>
2021-07-08 16:04:59 +0530

    sidebar fix

--- src/app/components/content/content.component.ts
+++ src/app/components/content/content.component.ts
@@ -7,6 +7,23 @@ import {Component, Input, OnInit} from '@angular/core';
 })
 export class ContentComponent implements OnInit {
   @Input('sideNavState') navState:boolean;
+  pages = [
+    {
+      name: 'Photos',
+      icon: 'folder',
+      link: '/home'
+    },
+    {
+      name: 'Recipes',
+      icon: 'folder',
+      link: '/home'
+    },
+    {
+      name: 'Work',
+      icon: 'folder',
+      link: '/home'
+    }
+  ];
   constructor() { }
 
   ngOnInit() {
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -3,12 +3,14 @@ import { MatSliderModule } from '@angular/material/slider';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
 
 const modules = [
   MatSliderModule,
   MatToolbarModule,
   MatIconModule,
-  MatSidenavModule
+  MatSidenavModule,
+  MatListModule
 ]
 
 @NgModule({
